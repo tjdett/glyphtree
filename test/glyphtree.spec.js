9c75bf2194b824d788f200d2046ef3d83cfd6d53
Tim Dettrick <t.dettrick@uq.edu.au>
2013-04-08 11:38:13 +1000

    Replacing JSDOM with Domino.

--- test/glyphtree.spec.js
+++ test/glyphtree.spec.js
@@ -1,63 +1,70 @@
 /* :indentSize=2:tabSize=2:noTabs=true: */
 
-var jquery = require('jquery-latest'),
-  jsdom = require('jsdom'),
+var domino = require('domino'),
   coffeescript = require("coffee-script");
-  
+
 // Use Chai for assertions
 require('chai').should();
-  
 
-  
 describe('GlyphTree', function() {
 
   describe("#load()", function() {
 
-    it("should load and render a tree", function(done) {
-      jsdom.env({
-        html: '<body><div id="test"></div></body>',
-        done: function(errors, window) {
-          var $ = loadJQuery(window),
-            glyphtree = loadGlyphTree(window);
-          var tree = glyphtree($('#test'));
-          tree.load([
+    it("should load and render a tree", function() {
+      var window = domino.createWindow('<body><div id="test"></div></body>'),
+        document = window.document,
+        $ = loadJQuery(window),
+        glyphtree = loadGlyphTree(window);
+      var tree = glyphtree($('#test'));
+      tree.load([
+        {
+          name: "root",
+          attributes: {
+            foo: "bar"
+          },
+          children: [
             {
-              name: "root",
-              attributes: {
-                foo: "bar"
-              },
+              name: "subfolder",
               children: [
                 {
-                  name: "subfolder",
-                  children: [
-                    {
-                      name: "file.txt"
-                    }
-                  ]
+                  name: "README"
+                },
+                {
+                  name: "file.txt"
                 }
               ]
             }
-          ]);
-          $('.glyphtree-node').should.have.lengthOf(3);
-          done();
+          ]
         }
-      });
+      ]);
+      // Three nodes (root, subfolder, README, file.txt)
+      $('.glyphtree-node').should.have.lengthOf(4);
+      // Three trees
+      $('.glyphtree-tree').should.have.lengthOf(3);
+      // Two leaf nodes
+      $('.glyphtree-leaf').should.have.lengthOf(2);
+
+      // Tree starts unexpanded
+      $('.glyphtree-expanded').should.have.lengthOf(0);
+      // Click a node
+      $('#test > .glyphtree-tree > .glyphtree-node').click();
+      $('.glyphtree-expanded').should.have.lengthOf(1);
     });
-    
+
   });
-  
+
 });
 
 // ## Utility functions
 
-// Load jQuery in JSDOM window
+// Load jQuery in mock DOM window
 function loadJQuery(window) {
   var jquery = require('jquery-latest');
   jquery.create(window);
   return window.jQuery;
 }
 
-// Load GlyphTree in JSDOM window
+// Load GlyphTree in mock DOM window
 function loadGlyphTree(window) {
   var glyphtree = require(__dirname+"/../src/glyphtree.coffee");
   glyphtree.create(window);
